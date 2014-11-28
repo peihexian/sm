cda8965f120d6104a8611b6a5ef016a1fa4de54f
peihexian <peihexian@qq.com>
2014-11-28 17:15:19 +0800

    初始导入

--- src/main/java/com/shinowit/framework/security/dao/UserDAO.java
+++ src/main/java/com/shinowit/framework/security/dao/UserDAO.java
@@ -20,17 +20,18 @@ public class UserDAO {
     }
 
     public Set<String> getUserRoles(String loginName) {
-        String sql = "select role from sys_user u, sys_role r,sys_user_to_role ur where u.login_name=? and u.user_code=ur.user_code and r.role_code=ur.role_code";
+        String sql = "select r.role_code from sys_user u, sys_role r,sys_user_role ur where u.login_name=? and u.user_id=ur.user_id and r.role_code=ur.role_code";
         return new HashSet(jt.queryForList(sql, String.class, loginName));
     }
 
     public Set<String> findPermissions(String loginName) {
-        String sql = "select permission from sys_user u, sys_role r, sys_menu p, sys_user_to_role ur, sys_role_to_menu rp where u.login_name=? and u.user_code=ur.user_code and r.role_code=ur.role_code and r.role_code=rp.role_code and p.menu_id=rp.menu_id";
+//        String sql = "select p.permission from sys_user u, sys_role r, sys_menu p, sys_user_role ur, sys_role_to_menu rp where u.login_name=? and u.user_id=ur.user_id and r.role_code=ur.role_code and r.role_code=rp.role_code and p.menu_code=rp.menu_code";
+        String sql = "select p.menu_code from sys_user u, sys_role r, sys_menu p, sys_user_role ur, sys_role_to_menu rp where u.login_name=? and u.user_id=ur.user_id and r.role_code=ur.role_code and r.role_code=rp.role_code and p.menu_code=rp.menu_code";
         return new HashSet(jt.queryForList(sql, String.class, loginName));
     }
 
     public SysUser findByUsername(String loginName) {
-        String sql = "select id, user_code,login_name, login_pass, valid from sys_user where login_name=?";
+        String sql = "select user_id, login_name, login_pass, status,real_name from sys_user where login_name=?";
         List<SysUser> userList = jt.query(sql, new BeanPropertyRowMapper(SysUser.class), loginName);
         if (userList.size() == 0) {
             return null;
