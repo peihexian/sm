c39b5cac7ca5e0e9523418d57f86c92b32402033
peihexian@qq.com <peihexian@qq.com>
2014-11-29 16:31:13 +0800

    none

--- web/js/main/main.js
+++ web/js/main/main.js
@@ -1,45 +1,5 @@
 Ext.BLANK_IMAGE_URL = GLOBAL_ROOT_PATH + '/img/s.gif';
 Ext.QuickTips.init();
-var start = {
-    id: 'start-panel',
-    title: '欢迎使用',
-    layout: 'fit',
-    bodyStyle: 'padding:25px',
-    html: '<img src="./img/bg.jpg"/>'
-};
-var themes = new Ext.data.SimpleStore({
-    fields: ['theme', 'css'],
-    data: [
-        ['默 认', 'ext-all.css'],
-        ['access', 'ext-all-access.css'],
-        ['gray', 'ext-all-gray.css']
-    ]
-});
-var color_change = new Ext.form.ComboBox({
-    triggerAction: "all",
-    fieldLabel: '切换皮肤',
-    forceSelection: true,
-    listAlign: 'center',
-    typeAhead: true,
-    emptyText: "切换皮肤",
-   // width: 140,
-    store: themes,
-    displayField: "theme",
-    valueField: "css",
-    mode: "local",
-    listeners: {
-        'select': function (e) {
-            var conboBoxValue = color_change.getValue();
-            var cp = new Ext.state.CookieProvider();
-            Ext.state.Manager.setProvider(cp);
-            cp.setCookie('color',conboBoxValue);
-            Ext.util.CSS.swapStyleSheet(
-                'theme',
-                './static/js/extjs/resources/css/' + conboBoxValue
-            );
-        }
-    }
-});
 Ext.onReady(
     function () {
         setTimeout(function () {
@@ -50,33 +10,83 @@ Ext.onReady(
                     split: true
                 },
                 items: [{
+                    region: "north",
                     collapsible:false,
                     border:false,
-                    region: "north",
-                    bodyStyle:'background: url(./img/top_img.jpg) #fff no-repeat;',
-                    height: 90
+                    height: 90,
+                    layout:'column',
+                    items: [{
+                        columnWidth: 1,
+                        border:false,
+                        height: 90,
+                        bodyStyle:'background: url(./img/top_img.jpg) #fff no-repeat;'
+                    },{
+                        xtype:'panel',
+                        border:false,
+                        height: 90,
+                        width: 250,
+                        style:'background: transparent;',
+                        dockedItems: [{
+                            xtype: 'toolbar',
+                            dock: 'bottom',
+                            ui: 'footer',
+                            style:'background: transparent;',
+                            defaults: {minWidth: 30},
+                            items: [
+                                { xtype: 'component', flex: 1 },
+                                { xtype: 'button', text: '退出' ,handler:function(){
+                                    Ext.Msg.confirm('系统注销', '你确定要退出系统吗?', function (btn) {
+                                        if (btn == 'yes') {
+                                            window.location.href = GLOBAL_ROOT_PATH+'/login/logout';
+                                        }
+                                    })
+                                }
+                                }
+                            ]
+                        }],
+                        items:[{
+                            xtype:'combobox',
+                            triggerAction: "all",
+                            fieldLabel: '切换皮肤',
+                            forceSelection: true,
+                            listAlign: 'center',
+                            typeAhead: true,
+                            emptyText: "切换皮肤",
+                            store: new Ext.data.SimpleStore({
+                                fields: ['theme', 'css'],
+                                data: [
+                                    ['默 认', 'ext-all.css'],
+                                    ['access', 'ext-all-access.css'],
+                                    ['gray', 'ext-all-gray.css'],
+                                    ['neptune', 'ext-all-neptune.css']
+                                ]
+                            }),
+                            displayField: "theme",
+                            valueField: "css",
+                            mode: "local",
+                            listeners: {
+                                'select': function (e) {
+                                    var conboBoxValue = e.getValue();
+                                    var cp = new Ext.state.CookieProvider();
+                                    Ext.state.Manager.setProvider(cp);
+                                    cp.setCookie('color',conboBoxValue);
+                                    Ext.util.CSS.swapStyleSheet(
+                                        'theme',
+                                        './static/js/extjs/resources/css/' + conboBoxValue
+                                    );
+                                }
+                            }
+                        }]
+                    }]
                 }, {
                     title: "菜单",
                     region: "west",
                     id: 'accordion-panel',
-                    width: 150,
+                    width: 205,
                     minSize: 150,
                     layout: 'border',
                     margins: '2 0 5 5',
                     maxSize: 250,
-                    tbar: [color_change],
-                    bbar: [{
-                        iconCls: 'icon-zhuxiao',
-                        text: '注销系统',
-                        handler: function () {
-                            Ext.Msg.confirm('系统注销', '你确定要注销系统吗?数据都保存了吗?', function (btn) {
-                                if (btn == 'yes') {
-                                    window.location.href = 'login.do?type=logout';
-                                }
-                            })
-
-                        }
-                    }],
                     defaults: {
                         border: false
                     },
@@ -115,7 +125,13 @@ Ext.onReady(
                     margins: '2 5 5 0',
                     activeItem: 0,
                     border: false,
-                    items: [start ] //unit, ssort, provider, p_spec, p_goods, p_goodsBrowse, ingoods, outgoods, inreport, outreport, org
+                    items: [{
+                        id: 'start-panel',
+                        title: '欢迎使用',
+                        layout: 'fit',
+                        bodyStyle: 'padding:25px; background-image: url(./img/bg.jpg); background-repeat: no-repeat;  background-attachment: fixed;  background-position: 100% 100%'
+                        //html: '<img src="./img/bg.jpg"/>'
+                    } ]
                 }]
             });
             var cp = new Ext.state.CookieProvider();
