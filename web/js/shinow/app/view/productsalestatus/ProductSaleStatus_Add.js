cba834d8401984a9c4ffd69f0d3260faa55a56b1
peihexian <peihexian@qq.com>
2014-12-09 10:13:20 +0800

    初始导入

--- web/js/shinow/app/view/productsalestatus/ProductSaleStatus_Add.js
+++ web/js/shinow/app/view/productsalestatus/ProductSaleStatus_Add.js
@@ -2,73 +2,89 @@
  * <p>
  * 商品促销状态字典 新增数据window
  * </p>
- * 
+ *
  * @author 代码生成器
  * @class app.view.productsalestatus.ProductSaleStatus_Add
  * @extends Ext.panel.Panel
  */
- 
- Ext.define('app.view.productsalestatus.ProductSaleStatus_Add', {
-	extend:'Ext.panel.Panel',
-	alias : 'widget.ProductSaleStatus_Add',
-	layout:'border',
-	width:400,
-	height:175,
-	plain: true,
-	border : false,
-	frame :false,
-	buttonAlign:'center',	
-	initComponent: function() {
-		this.items = [
-		{
-			xtype: 'form',
-			defaultType:'textfield',
-			labelAlign:'right',
-			width:'auto',
-			// height : 150,
-			// width:390,
-			frame : true,
-			buttonAlign:'center',
-			defaults : {
-				allowBlank : false,
-				autowidth : true,
-				labelAlign:'right',
-				height:20,
-				width : 299
-			},
-			items: [
 
-				{
-					maxLength:20,
-					name:'statusName',
-					maxLengthText:'促销状态名称不能多于20个字符',
-					fieldLabel:'促销状态名称'
-				},
+Ext.define('app.view.productsalestatus.ProductSaleStatus_Add', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.ProductSaleStatus_Add',
+    layout: 'border',
+    width: 380,
+    height: 110,
+    border: false,
+    frame: false,
+    buttonAlign: 'center',
+    initComponent: function () {
+        var me = this;
+        Ext.applyIf(me, {
+            items: [
+                {
+                    region: "center",
+                    xtype: 'form',
+                    defaultType: 'textfield',
+                    labelAlign: 'right',
+                    border: false,
+                    buttonAlign: 'center',
+                    defaults: {
+                        allowBlank: false,
+                        autowidth: true,
+                        margin:'5 5 5 5',
+                        labelAlign: 'right',
+                        height: 20,
+                        width: 299
+                    },
+                    items: [
+                        {
+                            maxLength: 20,
+                            name: 'statusName',
+                            maxLengthText: '促销状态名称不能多于20个字符',
+                            fieldLabel: '促销状态名称'
+                        },
 
-				{
-					name:'status',
-					fieldLabel:'状态'
-				},
+                        {
+                            xtype: 'combobox',
+                            name: 'status',
+                            fieldLabel: '状态',
+                            forceSelection: true,
+                            emptyText: "请选择",
+                            store: new Ext.data.SimpleStore({
+                                fields: ['key', 'value'],
+                                data: [
+                                    ['true', '启用'],
+                                    ['false', '未启用']
+                                ]
+                            }),
+                            displayField: "value",
+                            valueField: "key",
+                            mode: "local"
+                        },
 
-				{
-					maxLength:50,
-					name:'memo',
-					maxLengthText:'备注不能多于50个字符',
-					fieldLabel:'备注'
-				}	
-			]
-		}];
-		this.buttons = [
-			{
-				text: '保存',
-				action: 'save'
-			},
-			{
-				text: '关闭',
-				scope: this,
-				handler: function(btn){btn.up('window').close();}
-			}
-		];   
-		this.callParent(arguments);
-	}
+                        {
+                            maxLength: 50,
+                            name: 'memo',
+                            maxLengthText: '备注不能多于50个字符',
+                            fieldLabel: '备注'
+                        }
+                    ]
+                }]
+            ,
+            buttons: [
+                {
+                    text: '保存',
+                    action: 'save'
+                },
+                {
+                    text: '关闭',
+                    scope: this,
+                    handler: function (btn) {
+                        btn.up('window').close();
+                    }
+                }
+            ]
+        });
+        this.callParent(arguments);
+    }
 });
--- web/js/shinow/app/view/producttype/ProductType_Add.js
+++ web/js/shinow/app/view/producttype/ProductType_Add.js
@@ -2,78 +2,95 @@
  * <p>
  * 商品类别信息表 新增数据window
  * </p>
- * 
+ *
  * @author 代码生成器
  * @class app.view.producttype.ProductType_Add
  * @extends Ext.panel.Panel
  */
- 
- Ext.define('app.view.producttype.ProductType_Add', {
-	extend:'Ext.panel.Panel',
-	alias : 'widget.ProductType_Add',
-	layout:'border',
-	width:400,
-	height:175,
-	plain: true,
-	border : false,
-	frame :false,
-	buttonAlign:'center',	
-	initComponent: function() {
-		this.items = [
-		{
-			xtype: 'form',
-			defaultType:'textfield',
-			labelAlign:'right',
-			width:'auto',
-			// height : 150,
-			// width:390,
-			frame : true,
-			buttonAlign:'center',
-			defaults : {
-				allowBlank : false,
-				autowidth : true,
-				labelAlign:'right',
-				height:20,
-				width : 299
-			},
-			items: [
 
-				{
-					maxLength:10,
-					name:'typeCode',
-					maxLengthText:'商品类别编码不能多于10个字符',
-					fieldLabel:'商品类别编码'
-				},
+Ext.define('app.view.producttype.ProductType_Add', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.ProductType_Add',
+    layout: 'border',
+    width: 370,
+    height: 140,
+    border: false,
+    buttonAlign: 'center',
+    initComponent: function () {
+        var me = this;
+        Ext.applyIf(me, {
+            items: [
+                {
+                    region: "center",
+                    xtype: 'form',
+                    defaultType: 'textfield',
+                    labelAlign: 'right',
+                    border: false,
+                    buttonAlign: 'center',
+                    defaults: {
+                        allowBlank: false,
+                        autowidth: true,
+                        margin: '5 5 5 5',
+                        labelAlign: 'right',
+                        height: 20,
+                        width: 299
+                    },
+                    items: [
 
-				{
-					maxLength:50,
-					name:'typeName',
-					maxLengthText:'商品类别名称不能多于50个字符',
-					fieldLabel:'商品类别名称'
-				},
+                        {
+                            maxLength: 10,
+                            name: 'typeCode',
+                            maxLengthText: '商品类别编码不能多于10个字符',
+                            fieldLabel: '商品类别编码'
+                        },
 
-				{
-					name:'sortId',
-					fieldLabel:'排序编码'
-				},
+                        {
+                            maxLength: 50,
+                            name: 'typeName',
+                            maxLengthText: '商品类别名称不能多于50个字符',
+                            fieldLabel: '商品类别名称'
+                        },
 
-				{
-					name:'status',
-					fieldLabel:'状态'
-				}	
-			]
-		}];
-		this.buttons = [
-			{
-				text: '保存',
-				action: 'save'
-			},
-			{
-				text: '关闭',
-				scope: this,
-				handler: function(btn){btn.up('window').close();}
-			}
-		];   
-		this.callParent(arguments);
-	}
+                        {
+                            name: 'sortId',
+                            fieldLabel: '排序编码'
+                        },
+
+                        {
+                            xtype: 'combobox',
+                            name: 'status',
+                            fieldLabel: '状态',
+                            forceSelection: true,
+                            emptyText: "请选择",
+                            store: new Ext.data.SimpleStore({
+                                fields: ['key', 'value'],
+                                data: [
+                                    ['true', '启用'],
+                                    ['false', '未启用']
+                                ]
+                            }),
+                            displayField: "value",
+                            valueField: "key",
+                            mode: "local"
+                        }
+                    ]
+                }]
+            ,
+            buttons: [
+                {
+                    text: '保存',
+                    action: 'save'
+                },
+                {
+                    text: '关闭',
+                    scope: this,
+                    handler: function (btn) {
+                        btn.up('window').close();
+                    }
+                }
+            ]
+        });
+
+        this.callParent(arguments);
+    }
 });
