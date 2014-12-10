52ae7d06e4256d937bdb1a1c3bb5b7893a1203fc
peihexian <peihexian@qq.com>
2014-12-10 15:51:07 +0800

    初始导入

--- web/js/shinow/app/view/instock/InStock_Add.js
+++ web/js/shinow/app/view/instock/InStock_Add.js
@@ -2,129 +2,125 @@
  * <p>
  * 入库信息表 新增数据window
  * </p>
- * 
+ *
  * @author 代码生成器
  * @class app.view.instock.InStock_Add
  * @extends Ext.panel.Panel
  */
- 
- Ext.define('app.view.instock.InStock_Add', {
-	extend:'Ext.panel.Panel',
-	alias : 'widget.InStock_Add',
-	layout:'border',
-	width:400,
-	height:275,
-	plain: true,
-	border : false,
-	frame :false,
-	buttonAlign:'center',	
-	initComponent: function() {
-		var providerCode_store = Ext.create('app.store.Provider',{
-			pageSize : 1000,
-			autoLoad:true
-		});
-		var providerCode_combobox = Ext.create('Ext.form.ComboBox',{
-			name:'providerCode',
-			editable: false,
-			forceSelection: true,
-			labelAlign:'right',
-			fieldLabel:'供应商编码',
-			store:providerCode_store,
-			queryMode:'local',
-			displayField:'studyTypeName',
-			valueField:'providerCode',
-			allowBlank:false,
-			emptyText : '请选择'
-		});
-		var userId_store = Ext.create('app.store.SysUser',{
-			pageSize : 1000,
-			autoLoad:true
-		});
-		var userId_combobox = Ext.create('Ext.form.ComboBox',{
-			name:'userId',
-			editable: false,
-			forceSelection: true,
-			labelAlign:'right',
-			fieldLabel:'系统用户id',
-			store:userId_store,
-			queryMode:'local',
-			displayField:'studyTypeName',
-			valueField:'userId',
-			allowBlank:false,
-			emptyText : '请选择'
-		});
-		this.items = [
-		{
-			xtype: 'form',
-			defaultType:'textfield',
-			labelAlign:'right',
-			width:'auto',
-			// height : 150,
-			// width:390,
-			frame : true,
-			buttonAlign:'center',
-			defaults : {
-				allowBlank : false,
-				autowidth : true,
-				labelAlign:'right',
-				height:20,
-				width : 299
-			},
-			items: [
 
-				{
-					maxLength:6,
-					name:'providerCode',
-					maxLengthText:'供应商编码不能多于6个字符',
-					fieldLabel:'供应商编码'
-				},
+Ext.define('app.view.instock.InStock_Add', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.InStock_Add',
+    layout: 'border',
+    width: 400,
+    height: 275,
+    border: false,
+    buttonAlign: 'center',
+    initComponent: function () {
+        var me = this;
+        var providerCode_store = Ext.create('app.store.Provider', {
+            pageSize: 1000,
+            autoLoad: true
+        });
+        var userId_store = Ext.create('app.store.SysUser', {
+            pageSize: 1000,
+            autoLoad: true
+        });
+        Ext.applyIf(me, {
+            items: [
+                {
+                    region: "north",
+                    xtype: 'form',
+                    defaultType: 'textfield',
+                    labelAlign: 'right',
+                    border:false,
+                    buttonAlign: 'center',
+                    defaults: {
+                        allowBlank: false,
+                        autowidth: true,
+                        margin:'5 5 5 5',
+                        labelAlign: 'right',
+                        height: 20,
+                        width: 299
+                    },
+                    items: [
+                        {
+                            xtype: 'combobox',
+                            maxLength: 6,
+                            name: 'providerCode',
+                            editable: false,
+                            forceSelection: true,
+                            labelAlign: 'right',
+                            fieldLabel: '供应商',
+                            store: providerCode_store,
+                            queryMode: 'local',
+                            displayField: 'providerName',
+                            valueField: 'providerCode',
+                            allowBlank: false,
+                            emptyText: '请选择'
+                        },
+                        {
+                            xtype:'combobox',
+                            name: 'userId',
+                            editable: false,
+                            forceSelection: true,
+                            labelAlign: 'right',
+                            fieldLabel: '入库用户',
+                            store: userId_store,
+                            queryMode: 'local',
+                            displayField: 'realName',
+                            valueField: 'userId',
+                            allowBlank: false,
+                            emptyText: '请选择'
+                        },
 
-				{
-					name:'userId',
-					fieldLabel:'系统用户id'
-				},
+                        {
+                            name: 'inType',
+                            fieldLabel: '入库方式'
+                        },
 
-				{
-					name:'inType',
-					fieldLabel:'入库方式'
-				},
+                        {
+                            name: 'inTime',
+                            fieldLabel: '入库时间'
+                        },
 
-				{
-					name:'inTime',
-					fieldLabel:'入库时间'
-				},
+                        {
+                            maxLength: 20,
+                            name: 'handlerName',
+                            maxLengthText: '经手人不能多于20个字符',
+                            fieldLabel: '经手人'
+                        },
 
-				{
-					maxLength:20,
-					name:'handlerName',
-					maxLengthText:'经手人不能多于20个字符',
-					fieldLabel:'经手人'
-				},
+                        {
+                            name: 'totalMoney',
+                            fieldLabel: '入库金额'
+                        },
 
-				{
-					name:'totalMoney',
-					fieldLabel:'入库金额'
-				},
+                        {
+                            maxLength: 150,
+                            name: 'memo',
+                            maxLengthText: '备注不能多于150个字符',
+                            fieldLabel: '备注'
+                        }
+                    ]
+                }
+            ]
+            ,
+            buttons: [
+                {
+                    text: '保存',
+                    action: 'save'
+                },
+                {
+                    text: '关闭',
+                    scope: this,
+                    handler: function (btn) {
+                        btn.up('window').close();
+                    }
+                }
+            ]
+        });
 
-				{
-					maxLength:150,
-					name:'memo',
-					maxLengthText:'备注不能多于150个字符',
-					fieldLabel:'备注'
-				}	
-			]
-		}];
-		this.buttons = [
-			{
-				text: '保存',
-				action: 'save'
-			},
-			{
-				text: '关闭',
-				scope: this,
-				handler: function(btn){btn.up('window').close();}
-			}
-		];   
-		this.callParent(arguments);
-	}
+        this.callParent(arguments);
+    }
 });
